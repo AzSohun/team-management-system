d282c9cdc038704159d56381dcea128a0c86f9c8
Asaduzzaman Sohun <148855210+code-with-sohun@users.noreply.github.com>
2026-04-06 14:20:59 +0600

    Working User fetching from DB Api and add search params

---
+++ app/api/user/route.ts
@@ -0,0 +1,88 @@
+import { getCurrentUser } from "@/app/lib/auth";
+import { prisma } from "@/app/lib/db";
+import { Prisma, Role } from "@/generated/prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+
+
+
+export async function GET(request: NextRequest) {
+
+    try {
+
+        const user = await getCurrentUser();
+
+        if (!user) {
+            return NextResponse.json(
+                {
+                    error: "You are not authorized to access the users!"
+                },
+                {
+                    status: 401
+                }
+            )
+        };
+
+        const searchParams = request.nextUrl.searchParams;
+
+        const teamId = searchParams.get("teamId");
+        const role = searchParams.get("role");
+
+        const where: Prisma.UserWhereInput = {};
+
+        // Admin can see all
+        if (user.role === Role.ADMIN) {
+
+        }
+        // Maneager can see only all team members.
+        else if (user.role === Role.MANAGER) {
+            where.OR = [{ teamId: user.teamId }, { role: Role.USER }]
+        }
+        // User only can see same user 
+        else {
+            where.teamId = user.teamId;
+            where.role = { not: Role.ADMIN }
+        };
+
+
+        // Filter the teamId and role
+        if (teamId) {
+            where.teamId = teamId;
+        }
+        if (role) {
+            where.role = role as Role;
+        }
+
+
+        const users = await prisma.user.findMany({
+            where,
+            select: {
+                name: true,
+                email: true,
+                role: true,
+                team: {
+                    select: {
+                        id: true,
+                        name: true
+                    }
+                },
+                createdAt: true
+            },
+
+            orderBy: { createdAt: "desc" }
+        })
+
+        return NextResponse.json({ users });
+
+    } catch (error) {
+
+        console.error("Error: ", error);
+
+        return NextResponse.json(
+            {
+                error: "Internal server error!"
+            },
+            { status: 500 }
+        )
+    }
+}
+
